0e9495c2ace84f4a73d2cdee1d7f4cf6307bf6ec
Ondrej Sika <ondrej@ondrejsika.com>
2022-04-20 12:56:54 +0200

    cleanup: Remove unnecesarry provider definition

--- env/gitlab/main.tf
+++ env/gitlab/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    gitlab = {
-      source  = "gitlabhq/gitlab"
-      version = "3.13.0"
-    }
-  }
-}
-
 module "gitlab_project--k8s-demo" {
   source       = "../../modules/gitlab_project"
   namespace_id = 1
