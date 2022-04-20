73377520fbaf3465b168e0ca516e9bcc1f0d7210
Ondrej Sika <ondrej@ondrejsika.com>
2022-04-20 12:48:30 +0200

    feat(modules/gitlab_project): Create project module

---
+++ modules/gitlab_project/gitlab_project.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = "3.13.0"
+    }
+  }
+}
+
+variable "namespace_id" {
+  type        = number
+  description = "The namespace (group or user) of the project."
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the project."
+}
+
+variable "visibility_level" {
+  type        = string
+  description = "Use `public` or `private`"
+  default     = "public"
+}
+
+resource "gitlab_project" "this" {
+  namespace_id                     = var.namespace_id
+  name                             = var.name
+  visibility_level                 = var.visibility_level
+  initialize_with_readme           = false
+  merge_method                     = "ff"
+  default_branch                   = "master"
+  remove_source_branch_after_merge = true
+}
