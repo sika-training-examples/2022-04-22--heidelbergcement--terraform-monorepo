2814f4d0772f33ccfa19512d14d071ca23fab9f2
Ondrej Sika <ondrej@ondrejsika.com>
2022-04-20 14:42:31 +0200

    feat(modules/azurerm_resource_group): Create Azure Resource Group module

---
+++ modules/azurerm_resource_group/azurerm_resource_group.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.2.0"
+    }
+  }
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the resource group."
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+
+resource "azurerm_resource_group" "this" {
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+
+  name     = var.name
+  location = "West Europe"
+  tags     = var.tags
+}
+
+output "id" {
+  value = azurerm_resource_group.this.id
+}
