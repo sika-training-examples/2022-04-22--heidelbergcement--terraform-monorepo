df1084564b335a1aabd8a4522da715d997ab2958
Ondrej Sika <ondrej@ondrejsika.com>
2022-04-21 14:18:06 +0200

    feat(env/example-dev): Create storage for Terraform state

--- env/example-dev/terraform_state.tf
+++ env/example-dev/terraform_state.tf
@@ -3,3 +3,13 @@ module "azurerm_resource_group--terraform-state" {
   name   = "terraform_state"
   tags   = merge(local.tags_global, local.tags_billing_1)
 }
+
+module "azurerm_storage--terraform-state" {
+  source                 = "../../modules/azurerm_storage"
+  name                   = "terraform58nbdc"
+  tags                   = merge(local.tags_global, local.tags_billing_1)
+  azurerm_resource_group = module.azurerm_resource_group--terraform-state.azurerm_resource_group
+  containers = {
+    terraform = "private"
+  }
+}
